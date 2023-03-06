c1762aa353124cf8d5c5f4c3079575274bb6adb2
CoolKid52321 <119928093+CoolKid52321@users.noreply.github.com>
2023-03-05 21:28:49 -0500

    Add files via upload

---
+++ LuckyHubMain.lua
@@ -0,0 +1,10 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CoolKid52321/LuckyHubLoader/main/UILoader.lua"))();
+
+repeat task.wait() until game:IsLoaded();
+
+_G.start = tick();
+
+local ids = {
+    ["6229116934"] = "https://api.luarmor.net/files/v3/loaders/9cb3e8889bae49bfefef19e71c1fbbf4.lua",
+    ["2788229376"] = "https://api.luarmor.net/files/v3/loaders/a039c841e9c8c5f0821c71ac6bc08500.lua";
+}
